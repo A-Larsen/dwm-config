047fbbe06cea9ea86cfc8e3f17109b3509a50093
A-Larsen <austin.larsen@mailfence.com>
2025-12-03 22:01:49 -0500

    no right clicking tags

--- config.def.h
+++ config.def.h
@@ -148,7 +148,9 @@ static const Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+    // TODO:
+    // fix right clicking tag you're on causes dwm to freeze
+	// { ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
